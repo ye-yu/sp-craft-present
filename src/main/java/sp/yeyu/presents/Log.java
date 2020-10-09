571700518763a02b72e45d7ccefefc21e828ea74
Ye Yu <rafolwen98@gmail.com>
2020-10-09 15:31:48 +0800

    Prettify log

--- src/main/java/sp/yeyu/presents/Log.java
+++ src/main/java/sp/yeyu/presents/Log.java
@@ -3,11 +3,16 @@ package sp.yeyu.presents;
 public enum Log {
     INSTANCE;
 
-    private static final int LOOK_BACK = 4;
+    private static final int LOOK_BACK = 2;
 
     public void info(String message) {
         final StackTraceElement[] currentStackTrace = Thread.currentThread().getStackTrace();
-        final int lookBack = Math.max(currentStackTrace.length - Log.LOOK_BACK, 0);
-        CraftPresents.getInstance().getLogger().info("[CraftPresents:" + currentStackTrace[lookBack].getClassName() + "] " + message);
+        final int lookBack = Math.min(Log.LOOK_BACK, currentStackTrace.length);
+        CraftPresents.getInstance().getLogger().info(trimClassName(currentStackTrace[lookBack].getClassName()) + ": " + message);
+    }
+
+    public String trimClassName(final String className) {
+        final String[] split = className.split("\\.");
+        return split[Math.max(split.length - 1, 0)];
     }
 }
--- src/main/java/sp/yeyu/presents/Presents.java
+++ src/main/java/sp/yeyu/presents/Presents.java
@@ -5,45 +5,40 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashMap;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Random;
 import java.util.function.Supplier;
-import java.util.stream.IntStream;
 
 public enum Presents implements Supplier<ItemStack> {
     RED_BOX("https://textures.minecraft.net/texture/6cef9aa14e884773eac134a4ee8972063f466de678363cf7b1a21a85b7", PresentIngredients.RED.get());
 
     private static final Random random = new Random(System.currentTimeMillis());
-    private static final HashMap<Integer, Presents> map = new HashMap<>();
     private final Material dyeColor;
     private final String texture;
-    private final int itemHash;
     private final String namespaceName;
+    private Recipe recipe = null;
 
     Presents(String textureURL, Material dyeColor) {
         texture = textureURL;
         this.dyeColor = dyeColor;
-        itemHash = hashItemStack(new ItemStack[]{null, ItemUtil.INSTANCE.of(Material.PAPER), null,
-                ItemUtil.INSTANCE.of(Material.PAPER), ItemUtil.INSTANCE.of(this.dyeColor), ItemUtil.INSTANCE.of(Material.PAPER),
-                null, ItemUtil.INSTANCE.of(Material.PAPER), null
-        });
         namespaceName = String.join("", name().toLowerCase(Locale.ENGLISH).split("_"));
-        System.out.println(name() + " => " + itemHash);
-    }
-
-    static {
-        for (Presents value : values()) {
-            map.put(value.itemHash, value);
-        }
     }
 
     public ItemStack get() {
-        return NBTEditor.getHead(texture);
+        final ItemStack head = NBTEditor.getHead(texture);
+        head.setItemMeta(renamePresent(Objects.requireNonNull(head.getItemMeta())));
+        return head;
+    }
+
+    private ItemMeta renamePresent(final ItemMeta itemMeta) {
+        itemMeta.setDisplayName("Present");
+        return itemMeta;
     }
 
     public static int length() {
@@ -54,27 +49,14 @@ public enum Presents implements Supplier<ItemStack> {
         return values()[random.nextInt(Presents.length())];
     }
 
-    public static int hashItemStack(ItemStack [] items) {
-        return IntStream.range(0, items.length).map(n -> {
-            final ItemStack item = items[n];
-            final int dyeIndex = PresentIngredients.indexOf(item);
-            if (item == null || dyeIndex == -1) return 0;
-            else return (dyeIndex + 1) + n * (PresentIngredients.values().length + 1);
-        }).sum();
-    }
-
-    public static Presents getOrNull(int itemHash) {
-        return map.getOrDefault(itemHash, null);
-    }
-
-    public void craft(JavaPlugin plugin) {
+    protected void craft(JavaPlugin plugin) {
         final NamespacedKey key = new NamespacedKey(plugin, namespaceName);
         final ShapedRecipe recipe = new ShapedRecipe(key, get());
         recipe.shape(" P ", "PDP", " P ");
         recipe.setIngredient('P', Material.PAPER);
         recipe.setIngredient('D', dyeColor);
         Bukkit.addRecipe(recipe);
-        Objects.requireNonNull(Bukkit.getRecipe(key));
+        this.recipe = Objects.requireNonNull(Bukkit.getRecipe(key));
     }
 
     public static void craftPresent(JavaPlugin plugin) {
